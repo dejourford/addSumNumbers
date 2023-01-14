69e1b9be25c50dca62509cf7e32c053e25e46553
dejourford <dejourford@my.unt.edu>
2023-01-14 13:01:16 -0600

    declare variables and add js calculations

---
+++ script.js
@@ -0,0 +1,35 @@
+const numberOneBtn = document.querySelector('#one')
+const numberThreeBtn =  document.querySelector('#plusThree')
+const numberNineBtn = document.querySelector('#plusNine')
+const numberTwoBtn = document.querySelector('#minusTwo')
+let message = document.querySelector('.message')
+let result = 0
+
+const numberOneBtnValue = Math.floor(numberOneBtn.textContent)
+const numberThreeBtnValue = Math.floor(numberThreeBtn.textContent)
+const numberNineBtnValue = Math.floor(numberNineBtn.textContent)
+const numberTwoBtnValue = Math.floor(numberTwoBtn.textContent)
+
+
+
+
+
+numberOneBtn.addEventListener('click', function(){
+    result += numberOneBtnValue
+    message.textContent = `Result: ${result}`
+})
+
+numberThreeBtn.addEventListener('click', function(){
+    result += numberThreeBtnValue
+    message.textContent = `Result: ${result}`
+})
+
+numberNineBtn.addEventListener('click', function(){
+    result += numberNineBtnValue
+    message.textContent = `Result: ${result}`
+})
+
+numberTwoBtn.addEventListener('click', function(){
+    result += numberTwoBtnValue
+    message.textContent = `Result: ${result}`
+})
